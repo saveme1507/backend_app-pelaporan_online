2f834d7b9432aafa785fd9a1fbc8f61f06da0be4
saveme1507 <asepsoo55@gmail.com>
2020-07-22 00:45:02 +0700

    test git

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-define('BASE_URL', 'https://app-pelaporan.artisan-clothing.my.id');
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'hello word!';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello word!</h1>
+    <?php
+    define('BASE_URL', 'https://app-pelaporan.artisan-clothing.my.id');
+
+    echo 'hello word!';
+    ?>
+
+</body>
+
+</html>
